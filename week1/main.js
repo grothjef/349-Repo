549ed90ff2824d5aa2a3b4bcacf31fc7f1467e52
Jeff Groth <grothjef@gmail.com>
2014-05-20 19:40:11 -0400

    Almost Done

---
+++ week1/main.js
@@ -0,0 +1,14 @@
+/**
+ * @author groth_000
+ */
+function pressInfo()
+{
+	var content = document.getElementById("frame");
+	content.src = "info.html";
+}
+
+function pressNote()
+{
+	var content = document.getElementById("frame");
+	content.src = "note.html";
+}
